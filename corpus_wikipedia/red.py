7277b624a0f9bbe5d1c152927486a8b959e8b498
Gonzalo Martin Herrera Torena - Ens <gonzalo.herrera@pcunix63.fing.edu.uy>
2016-06-01 12:10:33 -0300

    Primer acercamiento

--- corpus_wikipedia/red.py
+++ corpus_wikipedia/red.py
@@ -1,10 +1,11 @@
 from vector_palabras import palabras_comunes, generar_vectores_iniciales
 import tensorflow as tf
 window_size = 5
-vector_size = 5
+vector_size = 150
+cant_palabras = 100002
 
 p = palabras_comunes("es-lexicon.txt")
-vectores = generar_vectores_iniciales(100002, vector_size)
+vectores = generar_vectores_iniciales(cant_palabras, vector_size)
 
 def obtener_matriz(oracion):
 	# resultado = tf.zeros([window_size, vector_size] )
@@ -14,9 +15,20 @@ def obtener_matriz(oracion):
 		vector = vectores[p.obtener_indice(palabra)]
 		matriz.append(vector)
 	return matriz
+
 sess = tf.InteractiveSession()
 oracion = ["La", "casa", "del", "gato"]
 matriz = obtener_matriz(oracion)
-I = tf.Variable(matriz)
+
+x = tf.placeholder(tf.float32, shape=[window_size, cant_palabras])
+
+
+wVectores = tf.Variable(vectores)
+
+y = tf.nn.softmax(tf.matmul(x,wVectores))
+
 sess.run(tf.initialize_all_variables())
 print I.eval()
+
+
+
