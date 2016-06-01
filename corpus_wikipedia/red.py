00de5cb7d605f9d9d2808577b979565ee51989b8
Gonzalo <gonzalo@localhost.localdomain>
2016-06-01 17:28:25 -0300

    Pruebas nuevas

--- corpus_wikipedia/red.py
+++ corpus_wikipedia/red.py
@@ -1,7 +1,7 @@
 from vector_palabras import palabras_comunes, generar_vectores_iniciales
 import tensorflow as tf
 window_size = 5
-vector_size = 150
+vector_size = 3
 cant_palabras = 100002
 
 p = palabras_comunes("es-lexicon.txt")
@@ -10,25 +10,25 @@ vectores = generar_vectores_iniciales(cant_palabras, vector_size)
 def obtener_matriz(oracion):
 	# resultado = tf.zeros([window_size, vector_size] )
 	matriz = []
-	for palabra in oracion:
-		print palabra
-		vector = vectores[p.obtener_indice(palabra)]
-		matriz.append(vector)
-	return matriz
+	for i in range (window_size):
+		matriz.append([0] * cant_palabras)
+	for i in range (window_size):
+		matriz[i][p.obtener_indice(oracion[i])] = 1
+	return tf.Variable(matriz)
 
 sess = tf.InteractiveSession()
-oracion = ["La", "casa", "del", "gato"]
+oracion = ["La", "casa", "del", "gato", "verde"]
 matriz = obtener_matriz(oracion)
 
-x = tf.placeholder(tf.float32, shape=[window_size, cant_palabras])
-
-
-wVectores = tf.Variable(vectores)
-
-y = tf.nn.softmax(tf.matmul(x,wVectores))
-
-sess.run(tf.initialize_all_variables())
-print I.eval()
+#x = tf.placeholder(tf.float32, shape=[window_size, cant_palabras])
+#
+#
+#wVectores = tf.Variable(vectores)
+#
+#y = tf.nn.softmax(tf.matmul(x,wVectores))
+#
+#sess.run(tf.initialize_all_variables())
+#print y.eval(feed_dict={x: matriz})
 
 
 
