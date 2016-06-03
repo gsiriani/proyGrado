f56d0308359e9c15b10d806573cfc0b78ba407cf
Gonzalo Martin Herrera Torena - Ens <gonzalo.herrera@pcunix66.fing.edu.uy>
2016-06-03 18:03:51 -0300

    Falta batch training

--- corpus_wikipedia/red.py
+++ corpus_wikipedia/red.py
@@ -1,10 +1,11 @@
 from vector_palabras import palabras_comunes
 from random import random
 import tensorflow as tf
-window_size = 5
-vector_size = 3
-cant_palabras = 100002
-unidades_ocultas_capa_2 = 500
+window_size = 5 # Cantidad de palabras en cada caso de prueba
+vector_size = 3 # Cantidad de features para cada palabra. Coincide con la cantidad de hidden units de la primer capa
+cant_palabras = 100002	# Cantidad de palabras consideradas en el diccionario
+unidades_ocultas_capa_2 = 5
+unidades_ocultas_capa_3 = 2
 
 p = palabras_comunes("es-lexicon.txt")
 
@@ -17,6 +18,14 @@ def generar_vectores_iniciales(cantidad, tamano):
 			vector.append(random())
 		lista_vectores.append(vector)
 	return lista_vectores
+
+def weight_variable(shape):
+	initial = tf.truncated_normal(shape, stddev=0.1)
+	return tf.Variable(initial)
+
+def bias_variable(shape):
+	initial = tf.constant(0.1, shape = shape)
+	return tf.Variable(initial)
 
 def obtener_matriz(oracion):
 	# resultado = tf.zeros([window_size, vector_size] )
@@ -34,18 +43,33 @@ oracion = ["La", "casa", "del", "gato", "verde"]
 matriz = obtener_matriz(oracion)
 
 x = tf.placeholder(tf.float32, shape=[window_size, cant_palabras])
+y_ = tf.placeholder(tf.float32, shape=[2])
 
 wVectores = tf.Variable(vectores)
 
-h_1 = tf.reshape(tf.matmul(x,wVectores),[vector_size * window_size])
+h_1 = tf.reshape(tf.matmul(x,wVectores),[-1,vector_size * window_size])
 
-w_capa_2 = tf.Variable(tf.zeros[unidades_ocultas_capa_2,vector_size * window_size])
-b_capa_2 = tf.Variable(tf.zeros[unidades_ocultas_capa_2])
+w_capa_2 = weight_variable([(vector_size * window_size), unidades_ocultas_capa_2])
+b_capa_2 = bias_variable([unidades_ocultas_capa_2])
 
-h_2 = tf.tanh(tf.matmul(w_capa_2, h_1) + b_capa_2)
+h_2 = tf.tanh(tf.matmul(h_1, w_capa_2) + b_capa_2)
 
+w_capa_3 = weight_variable([unidades_ocultas_capa_2, unidades_ocultas_capa_3])
+b_capa_3 = bias_variable([unidades_ocultas_capa_3])
 
+h_3 = tf.matmul(h_2,w_capa_3) + b_capa_3
+
+y = tf.nn.softmax(h_3)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices = [1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
 
 sess.run(tf.initialize_all_variables())
 
-print h_1.eval(feed_dict={x: matriz})
+for i in range(10):
+	print y.eval(feed_dict = {x : matriz})
+	sess.run(train_step, feed_dict = {x : matriz, y_ : [0,1]})
+	
+
+
