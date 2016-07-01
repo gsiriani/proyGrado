d28306a8cdb4b9530507e0e896a9882250740bbb
Gonzalo <gonzalo@localhost.localdomain>
2016-07-01 14:50:39 -0300

    Con fltrar para corpus

--- corpus_wikipedia/filtrar.py
+++ corpus_wikipedia/filtrar.py
@@ -2,15 +2,17 @@ from codecs import open, BOM_UTF8
 import csv
 import sys
 import io
-
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																														
 sys.stdin = io.TextIOWrapper(sys.stdin.buffer,encoding='utf-8');
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8');
 
 csv.field_size_limit(sys.maxsize)
 archivo = open("diez_porciento.csv", "r")
 lector = csv.reader(archivo, delimiter=' ')
-salida = open("diez_porciento_filtrado.csv", "w");
+salida = open("diez_porciento_filtrado.csv", "w");																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																												
 
-for r in lector:
-	if (len(r) == 13):
-		print(str(r), file = salida)
+i = 0
+for l in archivo:
+	r = l.split();
+	if (len(r) == 13):																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																												
+		print (l, end = '', file = salida)
