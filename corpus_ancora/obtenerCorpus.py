044dcc067c4c5a22341d8fb056df4ccd2007da00
Guillermo Siriani <guillebogus@gmail.com>
2016-10-05 17:10:31 -0300

    Se corrige la forma de obtener el corpus de Ancora

--- corpus_ancora/obtenerCorpus.py
+++ corpus_ancora/obtenerCorpus.py
@@ -1,8 +1,11 @@
+'''
+Obtengo todas las palabras de Ancora y su cantidad de ocurrencias.
+Se separan las palabras conectadas con '_'
+Las palabas con numeros se descartan
+'''
+
 import sys
 import json
-sys.path.append('.../myfreeling/APIs/python')
-import freeling
-import random
 import io
 from codecs import open, BOM_UTF8
 from glob import glob
@@ -12,8 +15,6 @@ print("Obteniendo Corpus...")
 # Inicializo diccionarios
 lexicon = {}
 numeros = {}
-fechas = {}
-desconocidos = {}
 
 # Funciones auxiliares
 # --------------------
@@ -27,69 +28,8 @@ def agregar_a_numeros(palabra):
     if not numeros.has_key(palabra):
         numeros[palabra] = 1
 
-def agregar_a_fechas(palabra):        
-    if not fechas.has_key(palabra):
-        fechas[palabra] = 1
-
-def agregar_a_desconocidos(palabra, tag):        
-    if not desconocidos.has_key(palabra):
-        desconocidos[palabra] = tag
-
-def es_fecha(palabra, tk, sp, morfo):
-    line = " ".join(palabra.split('_'))
-
-    # Divido la linea en tokens (palabras)
-    palabras_linea = tk.tokenize(line.lower())
-
-    # Agrupo en oraciones para analizar
-    oracion = sp.split(palabras_linea)[0]
-    oracion = morfo.analyze(oracion)
-    palabras = oracion.get_words()
-    analisis = palabras[0].get_analysis()
-    return analisis != () and "W" in str(analisis[0].get_tag())
-
-
-
-
-# Configuracion de freeling
-# -------------------------
-FREELINGDIR = "/usr/local";
-DATA = FREELINGDIR+"/share/freeling/"
-LANG = "es"
-
-freeling.util_init_locale("default")
-
-# Se crean opciones para analizador maco
-op= freeling.maco_options("es")
-op.set_data_files( "", 
-                   DATA + "common/punct.dat",
-                   DATA + LANG + "/dicc.src",
-                   DATA + LANG + "/afixos.dat",
-                   "",
-                   DATA + LANG + "/locucions.dat", 
-                   DATA + LANG + "/np.dat",
-                   DATA + LANG + "/quantities.dat",
-                   DATA + LANG + "/probabilitats.dat")
-
-# Se crea el analizador maco con las opciones precreadas
-morfo = freeling.maco(op)
-# Se setean los analisis requeridos. Solamente se usa deteccion de numeros y de fechas 
-morfo.set_active_options (False, # UserMap
-                         True, # NumbersDetection,
-                         False, #  PunctuationDetection,
-                         True, #  DatesDetection,  --> Setear a True para considerar fechas
-                         False, #  DictionarySearch,
-                         False, #  AffixAnalysis,
-                         False, #  CompoundAnalysis,
-                         False, #  RetokContractions,
-                         False, #  MultiwordsDetection,
-                         False, #  NERecognition,
-                         False, #  QuantitiesDetection,
-                         False) #  ProbabilityAssignment
-
-# Se crean tokenizador y splitter
-tk = freeling.tokenizer(DATA+LANG+"/tokenizer.dat")
-sp = freeling.splitter(DATA+LANG+"/splitter.dat")
+def has_num(palabra):
+  return any(char.isdigit() for char in palabra)
 
 
 # Estructura principal
@@ -98,55 +38,27 @@ i = 0 # Contador de palabras leidas
 # Recorro las oraciones (lineas) del archivo
 for line in open('ancora_oraciones.txt', encoding="utf-8"):
 
-    # Divido la linea en tokens (palabras)
-    palabras_linea = tk.tokenize(line.lower())
+  	# Recorro las palabras de la oracion
+    for token in line[:-3].split(' '):	# el [:-3] es para deshacerme del salto de linea al final de cada oracion
 
-    # Agrupo en oraciones para analizar
-    for oracion in sp.split(palabras_linea):
-        # Analizo la oracion          
-        oracion = morfo.analyze(oracion)
-        palabras = oracion.get_words()
+        # Obtengo palabras separadas por '_'
+        palabras = token.split('_')
 
-    # Recorro las palabras de la oracion
-        for palabra in palabras:  
-            # Actualizo la cantidad de palabras leidas
+        for palabra in palabras:
+          	# Actualizo la cantidad de palabras leidas
             i += 1
-            # Obtengo el analisis
-            analisis = palabra.get_analysis()
-            if analisis != ():
-                if ("Z" in str(analisis[0].get_tag())):   
-                    # la palabra es un numero
-                    if es_fecha(palabra.get_form(), tk, sp, morfo):
-                        agregar_a_fechas(palabra.get_form())
-                    else:
-                        agregar_a_numeros(palabra.get_form())
-                elif ("W" in str(analisis[0].get_tag())): 
-                    # la palabra es una fecha
-                    agregar_a_fechas(palabra.get_form())
-                else:
-                    # no se reconoce la etiqueta de la palabra
-                    agregar_a_desconocidos(palabra.get_form(), analisis[0].get_tag())
-            else:
-                # Agrego palabra al diccionario
-                agregar_a_lexicon(palabra.get_form())
 
-  	# # Recorro las palabras de la oracion
-   #  for palabra in line[:-3].split(' '):	# el [:-3] es para deshacerme del salto de linea al final de cada oracion
-   #    	# Actualizo la cantidad de palabras leidas
-   #      i += 1
-   #      if palabra.isdigit() and len(palabra) > 0:
-   #          agregar_a_numeros(palabra)
-   #      else:
-   #          agregar_a_lexicon(palabra.lower())
+            if has_num(palabra):
+                agregar_a_numeros(palabra)
+
+            elif len(palabra) > 0:
+                agregar_a_lexicon(palabra.lower())
  
 print("Ordenando palabras...")
 
 # Grabo los resultados en archivos de texto
 # ------------------------------------------
-
-# Palabras mas frecuentes
-# - - - - - - - - - - - -
-# Ordeno segun la frecuencia y me quedo con la cantidad deseada        
+ 
 top = []  
 for (w,freq) in lexicon.items():    
     top.append((freq, w))
@@ -163,16 +75,6 @@ open("lexicon_ancora.txt", "w").write(BOM_UTF8 + "\n".join(resultado).encode("ut
 # - - - - - - - - - - - - - - - - -
 numeros_aux = [w for (w,_) in numeros.items()]
 open("descarte/lexicon_ancora-numeros.txt", "w").write(BOM_UTF8 + "\n".join(numeros_aux).encode("utf-8"))
-
-# Palabras identificadas como fecha
-# - - - - - - - - - - - - - - - - -
-fechas_aux = [w for (w,_) in fechas.items()]
-open("descarte/es-lexicon_ancora-fechas.txt", "w").write(BOM_UTF8 + "\n".join(fechas_aux).encode("utf-8"))
-
-# Palabras con tag desconocida
-# - - - - - - - - - - - - - - 
-desconocidos_aux = [str(w) + ' ' + str(t) for (w,t) in desconocidos.items()]
-open("descarte/es-lexicon_ancora-desconocidos.txt", "w").write(BOM_UTF8 + "\n".join(desconocidos_aux).encode("utf-8"))
 
 
 # Calculo porcentaje de cobertura
