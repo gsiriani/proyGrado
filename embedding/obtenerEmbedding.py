fbdbc4794399e38f7633a303f7bbca264be59b40
Guillermo Siriani <guillebogus@hotmail.com>
2016-12-14 02:47:53 -0300

    Se corrije la obtencion y el calculo de ocurrencias del embedding

--- embedding/obtenerEmbedding.py
+++ embedding/obtenerEmbedding.py
@@ -3,20 +3,39 @@ from codecs import open, BOM_UTF8
 import re
 
 # Cargo lexicon
+print 'Cargando lexicon...'
 lexicon = []
-for linea in open('../lexicon/lexicon.txt', encoding="utf-8"):
+for linea in open('../lexicon/lexicon.txt', encoding="latin-1"):
     r = linea.split()
     if len(r) > 0:
         lexicon.append(r[0])
 
 
+print 'Cargando embedding completo...'
 # Cargo embedding pre-entrenado
 embedding_completo = {}
-for linea in open('./eswiki.0.3.vectors.150.txt'):
+palabras_embedding_completo = []
+palabras_corruptas = 0
+for linea in open('./eswiki.0.3.vectors.150.txt', encoding="latin-1"):
     r = linea.split()
     vector = r[1:]
     embedding_completo[r[0]] = vector
+    palabras_embedding_completo.append(r[0])
 
+    # try:
+    #     linea.decode('utf-8', 'strict')
+    #     r = linea.split()
+    #     vector = r[1:]
+    #     embedding_completo[r[0]] = vector
+    #     palabras_embedding_completo.append(r[0])
+    # except:
+    #     r = linea.split()
+    #     print r[0]
+    #     palabras_corruptas += 1
+
+print 'Se encontraron ' + str(palabras_corruptas) + ' palabras corruptas'
+
+print 'Filtrando palabras...'
 # Conservo las palabras en el lexicon
 embedding = []
 no_encontradas = []
@@ -32,9 +51,12 @@ for palabra in lexicon:
         no_encontradas.append(palabra)
 
 # Persisto las palabras
-open("embedding.txt", "w").write(BOM_UTF8 + "\n".join(embedding).encode("utf-8"))
-open("faltantes.txt", "w").write(BOM_UTF8 + "\n".join(no_encontradas).encode("utf-8"))
-open("lexicon.txt", "w").write(BOM_UTF8 + "\n".join(nuevo_lexicon).encode("utf-8"))
+print 'Persistiendo resultados...'
+open("embedding.txt", "w").write(BOM_UTF8 + "\n".join(embedding).encode("latin-1"))
+open("faltantes.txt", "w").write(BOM_UTF8 + "\n".join(no_encontradas).encode("latin-1"))
+open("lexicon.txt", "w").write(BOM_UTF8 + "\n".join(nuevo_lexicon).encode("latin-1"))
+
+open("palabras_embedding.txt", "w").write(BOM_UTF8 + "\n".join(palabras_embedding_completo).encode("latin-1"))
 
 print 'Se obtuvo el embedding de ' + str(len(embedding)) + ' palabras'
 print 'Se perdieron ' + str(len(no_encontradas)) + ' palabras en el proceso'
