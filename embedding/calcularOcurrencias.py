6f7d5fd515dc27a85cadadb48de88a15ecb8fc80
Guillermo Siriani <guillebogus@hotmail.com>
2017-02-10 18:25:54 -0300

    Calculado el porcentaje de ocurrencias de los embedding

--- embedding/calcularOcurrencias.py
+++ embedding/calcularOcurrencias.py
@@ -5,15 +5,26 @@ import io
 from codecs import open, BOM_UTF8
 import re
 
+# Funcion auxiliar
+def is_punct(palabra):
+    return (len(palabra) == 1 and not palabra.isalnum())
+
+
+ocurrencias_wiki = 0
+ocurrencias_ancora = 0
+
+
 # Cargo lexicon wikipedia
 corpus_wiki = {}
 total_ocurrencias_wiki = 0
-for linea in open('../lexicon/corpus_wikipedia/lexicon_wiki-frecuentes.txt', encoding="latin-1"):
+for linea in open('../lexicon/corpus_wikipedia/lexicon_wiki-total.txt', encoding="latin-1"):
     r = linea.split()
     if len(r) > 1:
         num = int(re.sub("[^0-9]", "", r[0]))
         corpus_wiki[r[1]] = num
         total_ocurrencias_wiki += num
+        if is_punct(r[1]): # Si es un signo de puntuacion lo considero cubierto
+            ocurrencias_wiki += num
 
 # Cargo lexicon ancora
 corpus_ancora = {}
@@ -23,41 +34,37 @@ for linea in open('../lexicon/corpus_ancora/lexicon_ancora.txt', encoding="latin
     num = int(re.sub("[^0-9]", "", r[0]))
     corpus_ancora[r[1]] = num
     total_ocurrencias_ancora += num
+    if is_punct(r[1]):  # Si es un signo de puntuacion lo considero cubierto
+        ocurrencias_ancora += num
 
-# Cargo lexicon ancora
+# Cargo lexicon embedding total
 lexicon = []
-for linea in open('./lexicon.txt', encoding="latin-1"):
+for linea in open('./lexicon_total.txt', encoding="latin-1"):
     r = linea.split()
     lexicon.append(r[0])
 
-ocurrencias_wiki = 0
-ocurrencias_ancora = 0
-
 for palabra in lexicon:
-    # if palabra in corpus_wiki:
-    #     ocurrencias_wiki += corpus_wiki[palabra]
-    if palabra in corpus_ancora:
+    if palabra in corpus_wiki and not is_punct(palabra):
+        ocurrencias_wiki += corpus_wiki[palabra]
+    if palabra in corpus_ancora and not is_punct(palabra):
         ocurrencias_ancora += corpus_ancora[palabra]
 
-faltantes = {}
-for (palabra, frec) in corpus_ancora.items():
-    if not palabra in lexicon:
-        faltantes[palabra] = frec
+# faltantes = {}
+# for (palabra, frec) in corpus_ancora.items():
+#     if not palabra in lexicon:
+#         faltantes[palabra] = frec
+#
+# # Ordeno segun la frecuencia
+# top = []
+# for (w, freq) in faltantes.items():
+#     top.append((freq, w))
+#
+# top = sorted(top, reverse=True)
+# resultado_tot = [str(f) + ' ' + w for (f,w) in top] # Almaceno palabras y cantidad de usos
+# # Guardo las palabras en un archivo de texto
+# open("frecuencia_faltantes.txt", "w").write(BOM_UTF8 + "\n".join(resultado_tot).encode("latin-1"))
 
-# Ordeno segun la frecuencia
-top = []
-for (w, freq) in faltantes.items():
-    top.append((freq, w))
+print 'Porcentaje de ocurrencias cubiertas en Wikipedia: ' + str(float(ocurrencias_wiki * 100 / total_ocurrencias_wiki)) + '%'
 
-top = sorted(top, reverse=True)
-resultado_tot = [str(f) + ' ' + w for (f,w) in top] # Almaceno palabras y cantidad de usos
-# Guardo las palabras en un archivo de texto
-open("frecuencia_faltantes.txt", "w").write(BOM_UTF8 + "\n".join(resultado_tot).encode("latin-1"))
-
-# print 'Porcentaje de cobertura Wikipedia: ' + str(float(len(lexicon) * 100 / len(corpus_wiki))) + '%'
-# print 'Porcentaje de ocurrencias cubiertas en Wikipedia: ' + str(
-#     float(ocurrencias_wiki * 100 / total_ocurrencias_wiki)) + '%'
-
-print 'Porcentaje de cobertura Ancora: ' + str(float(len(lexicon) * 100 / len(corpus_ancora))) + '%'
-print 'Porcentaje de ocurrencias cubiertas: ' + str(float(ocurrencias_ancora * 100 / total_ocurrencias_ancora)) + '%'
+print 'Porcentaje de ocurrencias cubiertas en Ancora: ' + str(float(ocurrencias_ancora * 100 / total_ocurrencias_ancora)) + '%'
 
