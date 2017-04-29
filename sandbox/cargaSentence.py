524d5fe59cd5a90504d09e1760c31b6ecb3e71af
Guillermo Siriani <guillebogus@hotmail.com>
2017-04-29 01:18:08 -0300

    Ejemplo corregido

--- sandbox/cargaSentence.py
+++ sandbox/cargaSentence.py
@@ -1,19 +1,12 @@
-path_proyecto = '/home/guille/proyGrado'
-
-import sys
-sys.path.append(path_proyecto)
 
 from random import uniform
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from script_auxiliares import print_progress
 
 window_size = 11 # Cantidad de palabras en cada caso de prueba
 unidades_ocultas_capa_2 = 300
 unidades_ocultas_capa_3 = 16 # SE MODIFICA PARA CADA PROBLEMA A RESOLVER
 
-archivo_corpus_entrenamiento = path_proyecto + "/sandbox/csv_prueba.csv"
+archivo_corpus_entrenamiento = "./csv_prueba.csv"
 
 
 
@@ -24,8 +17,11 @@ print 'Cargando casos de entrenamiento...'
 df = pd.read_csv(archivo_corpus_entrenamiento, delim_whitespace=True, skipinitialspace=True, header=None, quoting=3)
 
 a=df.at[0,0]
+print a
 b=eval(a)
 print b
+for x in b:
+    print x[0]
 
 
 
