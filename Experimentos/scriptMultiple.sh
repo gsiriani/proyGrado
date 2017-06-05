30e274e95bfe07c35f334a97b077af9a8f0b30e0
Guillermo Siriani <guillebogus@gmail.com>
2017-06-05 18:31:34 -0300

    Experimentos 12 al 16

---
+++ Experimentos/scriptMultiple.sh
@@ -0,0 +1,25 @@
+cd Experimento13
+echo 'Experimento 13 - chunking-iobesSeparado'
+python chunking-iobesSeparado.py > ./salida.txt
+cd ..
+cd Experimento14
+echo 'Experimento 14 - chunking'
+python chunking.py > ./salida.txt
+cd ..
+cd Experimento15
+echo 'Experimento 15 - ner-iobesSeparado'
+python ner-iobesSeparado.py > ./salida.txt
+cd ..
+cd Experimento16
+echo 'Experimento 16 - ner'
+python ner.py > ./salida.txt
+cd ..
+cd Experimento17
+echo 'Experimento 17 - chunking-iobesSeparado-150'
+python chunking-iobesSeparado-150.py > ./salida.txt
+cd ..
+cd Experimento18
+echo 'Experimento 18 - chunking-150'
+python chunking150.py > ./salida.txt
+cd ..
+echo 'Fin!!!!!!!!'
