443a58e600cc7ee94b1fdbf2279bcc9ac5b64837
Guillermo Siriani <guillebogus@gmail.com>
2017-06-25 18:56:23 -0300

    Evaluacion experimento 26 y armado experimento 28

---
+++ Experimentos/Experimento28/scriptMultiple.sh
@@ -0,0 +1,19 @@
+cd Chunking
+cd lr_0.1
+pwd
+python chunking.py > salida.txt
+cd ..
+cd lr_0.03
+pwd
+python chunking.py > salida.txt
+cd ..
+cd ..
+cd Ner
+cd lr_0.1
+pwd 
+python ner.py > salida.txt
+cd ..
+cd lr_0.07
+pwd
+python ner.py > salida.txt
+echo 'FIN!!!'
