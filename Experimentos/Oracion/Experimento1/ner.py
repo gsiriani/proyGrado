35dd0d4a00c68c17a2aabd0ed730a2731ee5b083
Guillermo Siriani <guillermo.siriani@fing.edu.uy>
2017-09-25 17:38:47 -0300

    Avances en convolutiva

--- Experimentos/Oracion/Experimento1/ner.py
+++ Experimentos/Oracion/Experimento1/ner.py
@@ -21,15 +21,15 @@ from codecs import open, BOM_UTF8
 
 vector_size = 50 # Cantidad de features a considerar por palabra
 unidades_ocultas_capa_2 = 300
-unidades_ocultas_capa_3 = 16 # SE MODIFICA PARA CADA PROBLEMA A RESOLVER
+unidades_ocultas_capa_3 = 24 # SE MODIFICA PARA CADA PROBLEMA A RESOLVER
 
-archivo_embedding = path_proyecto + "/embedding/embedding_ordenado.txt"
-archivo_corpus_entrenamiento = path_proyecto + '/corpus/Oracion/Entrenamiento/ner_training.csv'
-archivo_corpus_pruebas = path_proyecto + '/corpus/Oracion/Pruebas/ner_pruebas.csv'
+archivo_embedding = path_proyecto + "/embedding/lexicon_total.txt"
+archivo_corpus_entrenamiento = path_proyecto + '/corpus/Oracion/Entrenamiento/chunking_training.csv'
+archivo_corpus_pruebas = path_proyecto + '/corpus/Oracion/Pruebas/chunking_pruebas.csv'
 
 
 log = 'Log de ejecucion:\n-----------------\n'
-log += '\nTarea: POS'
+log += '\nTarea: Chunking'
 log += '\nModelo de red: Oracion'
 log += '\nEmbedding inicial: Aleatorio'
 log += '\nOptimizer: adam'
@@ -45,22 +45,23 @@ print 'Cantidad de palabras consideradas: ' + str(cant_palabras)
 
 main_input = Input(shape=(None,), name='main_input')
 
+aux_input_layer = Input(shape=(None,1), name='aux_input')
+
 # https://blog.keras.io/using-pre-trained-word-embeddings-in-a-keras-model.html
 embedding_layer = Embedding(input_dim=cant_palabras, output_dim=vector_size, embeddings_initializer='uniform',
                             trainable=True)(main_input)
 
-convolutive_layer = Conv1D(filters=unidades_ocultas_capa_2, kernel_size=5)(embedding_layer)
+concat_layer = Concatenate()([embedding_layer, aux_input_layer])
+
+convolutive_layer = Conv1D(filters=unidades_ocultas_capa_2, kernel_size=5)(concat_layer)
+#convolutive_layer = Conv1D(filters=unidades_ocultas_capa_2, kernel_size=5)(embedding_layer)
 
 x_layer = GlobalMaxPooling1D()(convolutive_layer)
-
-aux_input_layer = Input(shape=(150,), name='aux_input')
-
-concat_layer = Concatenate()([x_layer, aux_input_layer])
 
 second_layer = Dense(units=unidades_ocultas_capa_2,
                      use_bias=True,
                      kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.1, seed=None),
-                     bias_initializer=Constant(value=0.1))(concat_layer)
+                     bias_initializer=Constant(value=0.1))(x_layer)
 
 y_layer = Activation("tanh")(second_layer)
 
@@ -69,23 +70,13 @@ third_layer = Dense(units=unidades_ocultas_capa_3,
                     kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.1, seed=None),
                     bias_initializer=Constant(value=0.1))(y_layer)
 
-softmax_layer = Activation("softmax")(third_layer)
+softmax_layer = Activation("softmax", name='softmax_layer')(third_layer)
 
 
 # Agrego las capas al modelo
 
 model = Model(inputs=[main_input, aux_input_layer], outputs=[softmax_layer])
-
-
-
-# model.add(embedding_layer)
-# model.add(convolutive_layer)
-# model.add(Concatenate())
-# model.add(GlobalMaxPooling1D())
-# model.add(second_layer)
-# model.add(Activation("tanh"))
-# model.add(third_layer)
-# # model.add(Activation("softmax"))
+#model = Model(inputs=[main_input], outputs=[softmax_layer])
 
 
 # Compilo la red
@@ -113,8 +104,15 @@ with open(archivo_corpus_entrenamiento, 'rb') as archivo_csv:
 # x_train = np.array(df.iloc[:largo,:-unidades_ocultas_capa_3])
 # y_train = np.array(df.iloc[:largo,-unidades_ocultas_capa_3:])
 
-x_train_a = [l[1:] for l in x_train]
-x_train_b = [ [i-l[0] for i in range(len(l))] for l in x_train] # Matriz que almacenara distancias a la palabra a analizar
+x_train_a = [np.array(l[1:]) for l in x_train]
+x_train_b = [ np.array([np.array([i-l[0]]) for i in range(len(l)-1)]) for l in x_train] # Matriz que almacenara distancias a la palabra a analizar
+x_train_a = np.array(x_train_a[:51])
+x_train_b = np.array(x_train_b[:51])
+y_train = np.array(y_train[:51])
+
+for l in range(len(x_train_a)):
+    if len(x_train_a[l]) <> len(x_train_b[l]):
+        print 'Discrepancia en linea: ' + str(l)
 
 
 print 'Cargando casos de prueba...' 
@@ -129,7 +127,7 @@ with open(archivo_corpus_pruebas, 'rb') as archivo_csv:
         y_test.append([int(x) for x in linea[-unidades_ocultas_capa_3:]])
 
 x_test_a = [l[1:] for l in x_test]
-x_test_b = [ [i-l[0] for i in range(len(l))] for l in x_test] # Matriz que almacenara distancias a la palabra a analizar
+x_test_b = [ [[i-l[0]] for i in range(len(l)-1)] for l in x_test] # Matriz que almacenara distancias a la palabra a analizar
 
 duracion_carga_casos = time.time() - inicio_carga_casos
 
@@ -138,7 +136,8 @@ print 'Entrenando...'
 inicio_entrenamiento = time.time()
 
 early_stop = EarlyStopping(monitor='val_acc', min_delta=0, patience=3, verbose=0, mode='auto')
-history = model.fit(main_input=x_train_a, aux_input_layer=x_train_b, y=y_train, epochs=10, batch_size=25, callbacks=[early_stop], verbose=2)
+history = model.fit({'main_input': x_train_a, 'aux_input': x_train_b}, {'softmax_layer': y_train}, epochs=10, batch_size=25, verbose=2)
+#history = model.fit({'main_input': x_train_a}, {'softmax_layer': y_train}, epochs=10, batch_size=25, verbose=2)
 duracion_entrenamiento = time.time() - inicio_entrenamiento
 
 
@@ -146,34 +145,34 @@ duracion_entrenamiento = time.time() - inicio_entrenamiento
 log += '\n\nTiempo de carga de casos de Entrenamiento/Prueba: {0} hs, {1} min, {2} s'.format(int(duracion_carga_casos/3600),int((duracion_carga_casos % 3600)/60),int((duracion_carga_casos % 3600) % 60))
 log += '\nDuracion del entrenamiento: {0} hs, {1} min, {2} s'.format(int(duracion_entrenamiento/3600),int((duracion_entrenamiento % 3600)/60),int((duracion_entrenamiento % 3600) % 60))
 
-log += '\n\nAccuracy entrenamiento inicial: ' + str(history.history['acc'][0])
-log += '\nAccuracy entrenamiento final: ' + str(history.history['acc'][-1])
-log += '\n\nAccuracy validacion inicial: ' + str(history.history['val_acc'][0])
-log += '\nAccuracy validacion final: ' + str(history.history['val_acc'][-1])
+# log += '\n\nAccuracy entrenamiento inicial: ' + str(history.history['acc'][0])
+# log += '\nAccuracy entrenamiento final: ' + str(history.history['acc'][-1])
+# log += '\n\nAccuracy validacion inicial: ' + str(history.history['val_acc'][0])
+# log += '\nAccuracy validacion final: ' + str(history.history['val_acc'][-1])
 
-log += '\n\nLoss entrenamiento inicial: ' + str(history.history['loss'][0])
-log += '\nLoss entrenamiento final: ' + str(history.history['loss'][-1])
-log += '\n\nLoss validacion inicial: ' + str(history.history['val_loss'][0])
-log += '\nLoss validacion final: ' + str(history.history['val_loss'][-1])
+# log += '\n\nLoss entrenamiento inicial: ' + str(history.history['loss'][0])
+# log += '\nLoss entrenamiento final: ' + str(history.history['loss'][-1])
+# log += '\n\nLoss validacion inicial: ' + str(history.history['val_loss'][0])
+# log += '\nLoss validacion final: ' + str(history.history['val_loss'][-1])
 
-#print log
-open("log.txt", "w").write(BOM_UTF8 + log)
+# #print log
+# open("log.txt", "w").write(BOM_UTF8 + log)
 
-# list all data in history
-print(history.history.keys())
-# summarize history for accuracy
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+# # list all data in history
+# print(history.history.keys())
+# # summarize history for accuracy
+# plt.plot(history.history['acc'])
+# plt.plot(history.history['val_acc'])
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+# # summarize history for loss
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
