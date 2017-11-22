f67ae7fe58811b4337b813002a3b8b8b04216606
gherrera <gonzalo.herrera@quanam.com>
2017-11-22 16:32:42 -0300

    Analizador de ner ventana

---
+++ generarCasos/Otros/analizar_ner_ventana.py
@@ -0,0 +1,58 @@
+import sys
+import re
+import os
+
+separador = ","
+largo_ventana = 11
+clave = (largo_ventana + 1) / 2
+
+def list_to_str(vector):
+	salida = ""
+	primero = True
+	for p in vector:
+		if primero:
+			salida = str(p)
+			primero = False
+		else:
+			salida += separador + str(p)
+	return salida
+
+def es_out(vector):
+	out = True
+	for i in range(len(vector) - 1):
+		if vector[i] != "0":
+			out = False
+	return out
+
+lista_palabras = open(sys.argv[1],"r")
+a_entrada = open(sys.argv[2],"r")
+a_salida = open(sys.argv[3],"w")
+
+tags = {}
+casos_ner = 0
+
+diccionario = []
+for linea in lista_palabras:
+	p = linea.replace("\n","")
+	diccionario.append[p]
+
+lista_palabras.close()
+
+for linea in a_entrada:
+	lista = linea.replace("\n","").split(separador)
+	etiquetas = lista[largo_ventana:]
+	if not es_out(etiquetas):
+		casos_ner += 1
+		etiqueta = list_to_str(etiquetas)
+		if etiqueta not in tags:
+			tags[etiqueta] = {}
+		if lista[clave] not in tags[etiqueta]:
+			tags[etiqueta].append(int(lista[clave]))
+for tag in tags:
+	salida.write(tag + "\n\n==============================\n\n")
+	for palabra in tags[tag]:
+		salida.write(diccionario[palabra] + "\n")
+	salida.write("\n----------------------------------\n\n")
+salida.write(str(casos_ner))
+a_entrada.close()
+a_salida.close()
