b7e5390ca546c531a99a7a781bd55777b9144f06
Guillermo Siriani <guillebogus@gmail.com>
2018-02-16 19:55:35 -0300

    Resultados de Testinggit add .!

--- Experimentos/Definitivos/Desarrollo/3/ejecutar_testing.sh
+++ Experimentos/Definitivos/Desarrollo/3/ejecutar_testing.sh
@@ -154,108 +154,108 @@ cd MacroChunking
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py macrochunking a > salida_testing.txt
+#_python ../../testing.py macrochunking a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py macrochunking p > salida_testing.txt
+#_python ../../testing.py macrochunking p > salida_testing.txt
 cd ..
 cd ..
 cd MicroChunking
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py microchunking a > salida_testing.txt
+#_python ../../testing.py microchunking a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py microchunking p > salida_testing.txt
+#_python ../../testing.py microchunking p > salida_testing.txt
 cd ..
 cd ..
 cd Ner
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py ner a > salida_testing.txt
+#_python ../../testing.py ner a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py ner p > salida_testing.txt
+#_python ../../testing.py ner p > salida_testing.txt
 cd ..
 cd ..
 cd Pos
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py pos a > salida_testing.txt
+#_python ../../testing.py pos a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py pos p > salida_testing.txt
+#_python ../../testing.py pos p > salida_testing.txt
 cd ..
 cd ..
 cd Srl
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing_srl.py srl a > salida_testing.txt
+#_python ../../testing_srl.py srl a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing_srl.py srl p > salida_testing.txt
+#_python ../../testing_srl.py srl p > salida_testing.txt
 cd ..
 cd ..
 cd St_Reducido
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py supertag_reducido a > salida_testing.txt
+#_python ../../testing.py supertag_reducido a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py supertag_reducido p > salida_testing.txt
+#_python ../../testing.py supertag_reducido p > salida_testing.txt
 cd ..
 cd ..
 cd St_Completo
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py supertag_completo a > salida_testing.txt
+#_python ../../testing.py supertag_completo a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py supertag_completo p > salida_testing.txt
+#_python ../../testing.py supertag_completo p > salida_testing.txt
 cd ..
 cd ..
 cd St_Reducido_Compactado
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py supertag_reducido_compactado a > salida_testing.txt
+#_python ../../testing.py supertag_reducido_compactado a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py supertag_reducido_compactado p > salida_testing.txt
+#_python ../../testing.py supertag_reducido_compactado p > salida_testing.txt
 cd ..
 cd ..
 cd St_Completo_Compactado
 cd Aleatorio
 pwd
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py supertag_completo_compactado a > salida_testing.txt
+#_python ../../testing.py supertag_completo_compactado a > salida_testing.txt
 cd ..
 cd Precalculado
 pwd 
 date "+%H:%M:%S   %d/%m/%y"
-python ../../testing.py supertag_completo_compactado p > salida_testing.txt
+#_python ../../testing.py supertag_completo_compactado p > salida_testing.txt
 cd ..
 cd ..
 cd Combinadas
---
+++ Experimentos/Definitivos/Desarrollo/4-Separadas/ejecutar_testing.sh
@@ -0,0 +1,214 @@
+cd Ventana
+cd MacroChunking
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py macrochunking a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py macrochunking p > salida_testing.txt
+cd ..
+cd ..
+cd MicroChunking
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py microchunking a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py microchunking p > salida_testing.txt
+cd ..
+cd ..
+cd Ner
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py ner a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py ner p > salida_testing.txt
+cd ..
+cd ..
+cd Pos
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py pos a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py pos p > salida_testing.txt
+cd ..
+cd ..
+cd Supertag
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py supertag a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py supertag p > salida_testing.txt
+cd ..
+cd ..
+cd Supertag_Compacto
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py supertag_compacto a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py supertag_compacto p > salida_testing.txt
+cd ..
+cd ..
+cd Combinadas
+cd Supertag
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../../multired_testing.py 1 a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd
+date "+%H:%M:%S   %d/%m/%y" 
+#_python ../../../multired_testing.py 1 p > salida_testing.txt
+cd ..
+cd ..
+cd Supertag_Compacto
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../../multired_testing.py 0 a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd
+date "+%H:%M:%S   %d/%m/%y" 
+#_python ../../../multired_testing.py 0 p > salida_testing.txt
+cd ..
+cd ..
+cd ..
+cd ..
+echo 'FIN VENTANA!!!'
+
+
+cd Sentencia
+cd MacroChunking
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py macrochunking a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py macrochunking p > salida_testing.txt
+cd ..
+cd ..
+cd MicroChunking
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py microchunking a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py microchunking p > salida_testing.txt
+cd ..
+cd ..
+cd Ner
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py ner a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py ner p > salida_testing.txt
+cd ..
+cd ..
+cd Pos
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py pos a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py pos p > salida_testing.txt
+cd ..
+cd ..
+cd Srl
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing_srl.py srl a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing_srl.py srl p > salida_testing.txt
+cd ..
+cd ..
+cd Supertag
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py supertag a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+#_python ../../testing.py supertag p > salida_testing.txt
+cd ..
+cd ..
+cd Supertag_Compacto
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+python ../../testing.py supertag_compacto a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd 
+date "+%H:%M:%S   %d/%m/%y"
+python ../../testing.py supertag_compacto p > salida_testing.txt
+cd ..
+cd ..
+cd Combinadas
+cd Supertag
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+python ../../../multired_testing.py 1 a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd
+date "+%H:%M:%S   %d/%m/%y" 
+python ../../../multired_testing.py 1 p > salida_testing.txt
+cd ..
+cd ..
+cd Supertag_Compacto
+cd Aleatorio
+pwd
+date "+%H:%M:%S   %d/%m/%y"
+python ../../../multired_testing.py 0 a > salida_testing.txt
+cd ..
+cd Precalculado
+pwd
+date "+%H:%M:%S   %d/%m/%y" 
+python ../../../multired_testing.py 0 p > salida_testing.txt
+cd ..
+cd ..
+echo 'FIN!!!'
